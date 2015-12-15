212d539e5811bd0809c0fbc14d9cfea62a06f612
Chubuntu Arciniega <arciniega.chubuntu@gmail.com>
2015-12-15 14:48:33 -0700

    Filto por dia en pantallas Factura - Orden - Oferta - back

--- html/scripts/busqueda_facturas.js
+++ html/scripts/busqueda_facturas.js
@@ -1,5 +1,20 @@
 // When document is ready: this gets fired before body onload <img src="http://blogs.digitss.com/wp-includes/images/smilies/simple-smile.png" alt=":)" class="wp-smiley" style="height: 1em; max-height: 1em;" />
 $(document).ready(function(){
+    $( "#busqueda" ).ready(function() {
+        $buscar = document.getElementById('busqueda').value
+  // When value of the input is not blank
+        if( $buscar != "")
+        {
+            // Show only matching TR, hide rest of them
+            $("#lista tbody>tr").hide();
+            $("#lista td:contains-ci('" + $buscar + "')").parent("tr").show();
+        }
+        else
+        {
+            // When there is no input or clean again, show everything back
+            $("#lista tbody>tr").show();
+        }
+});
     // Write on keyup event of keyword input element
     $("#busqueda").keyup(function(){
         // When value of the input is not blank
